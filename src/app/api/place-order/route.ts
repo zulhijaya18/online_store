4759fd1e8c5cdfdde575df9091e1191f02ccb613
zulhijaya18 <32.zulhijaya@gmail.com>
2024-12-07 22:54:36 +0800

    update place-order logic

--- src/app/api/place-order/route.ts
+++ src/app/api/place-order/route.ts
@@ -51,6 +51,17 @@ export async function POST(
     const estimateMinWeight = estimateWeightPerPackage - marginWeight
     const estimateMaxWeight = estimateWeightPerPackage + marginWeight
 
+
+    const calculateWeightPriceRatio = ({totalPrice, totalWeight}: Package) => {
+        const weight = totalWeight / estimateWeightPerPackage
+        const price = totalPrice / estimateMinPrice
+
+        return {
+            weight,
+            price
+        }
+    }
+
     for (let i = 0; i < packagesCount; i++) {
         packages.push({
             totalWeight: 0,
@@ -64,23 +75,90 @@ export async function POST(
 
     console.log(selectedItems)
 
+    const sortByWeightDesc = (a: Product, b: Product) => {
+        return Number(b.weight) - Number(a.weight)
+    }
+    const sortByPriceDesc = (a: Product, b: Product) => {
+        return Number(b.price) - Number(a.price)
+    }
+
+    let packagesIsEqual = false
     while(selectedItems.length > 0) {
-        const currentItem = selectedItems.shift()
-        if (!currentItem) {
-            break
-        }
-        let addedToExistingPackage = false
         for (const packageItem of packages) {
-            const estimateTotalPackageWeight = Number(packageItem.totalWeight) + Number(currentItem.weight)
-            if ((Number(currentItem.price) + packageItem.totalPrice) < 250 && (estimateTotalPackageWeight <= estimateWeightPerPackage || packageItem.items.length === 0 || selectedItems.length === 0)) {
+            if (packageItem.items.length === 0) {
+                selectedItems.sort(sortByWeightDesc)
+                const currentItem = selectedItems.shift()
+                if (!currentItem) {
+                    break
+                }
                 packageItem.items.push(currentItem)
                 packageItem.totalWeight += Number(currentItem.weight)
                 packageItem.totalPrice += Number(currentItem.price)
-                addedToExistingPackage = true
-                break
+                console.log(currentItem)
+            }
+            
+            let index = 0
+            let isLooping = true
+            while(isLooping) {
+                const { weight: weightRatio, price: priceRatio } = calculateWeightPriceRatio(packageItem)
+                if (weightRatio > priceRatio) {
+                    selectedItems.sort(sortByPriceDesc)
+                }else {
+                    selectedItems.sort(sortByWeightDesc)
+                }
+                const currentItem = selectedItems[index]
+                if (!currentItem) {
+                    break
+                }
+                const estimateTotalPackageWeight = Number(packageItem.totalWeight) + Number(currentItem.weight)
+                if ((Number(currentItem.price) + packageItem.totalPrice) < 250 && (estimateTotalPackageWeight <= estimateWeightPerPackage || packagesIsEqual)) {
+                    console.log(estimateTotalPackageWeight, packagesIsEqual)
+                    packageItem.items.push(currentItem)
+                    packageItem.totalWeight += Number(currentItem.weight)
+                    packageItem.totalPrice += Number(currentItem.price)
+                    console.log(currentItem)
+                    selectedItems.splice(selectedItems.indexOf(currentItem), 1)
+                } else {
+                    index++
+                    if (index >= selectedItems.length) {
+                        isLooping = false
+                    }
+                }
             }
         }
+        if (packagesIsEqual && selectedItems.length > 0) {
+            const { weight, price } = selectedItems[0]
+            packages.push({
+                totalWeight: Number(weight),
+                totalPrice: Number(price),
+                courierPrice: 0,
+                items: [selectedItems[0]]
+            })
+            selectedItems.shift()
+        }
+        packagesIsEqual = true
     }
+
+    // while(selectedItems.length > 0) {
+    //     const currentItem = selectedItems.shift()
+    //     if (!currentItem) {
+    //         break
+    //     }
+    //     let addedToExistingPackage = false
+    //     for (const packageItem of packages) {
+    //         const estimateTotalPackageWeight = Number(packageItem.totalWeight) + Number(currentItem.weight)
+    //         if ((Number(currentItem.price) + packageItem.totalPrice) < 250 && (estimateTotalPackageWeight <= estimateWeightPerPackage || packageItem.items.length === 0 || selectedItems.length === 0)) {
+    //             packageItem.items.push(currentItem)
+    //             packageItem.totalWeight += Number(currentItem.weight)
+    //             packageItem.totalPrice += Number(currentItem.price)
+    //             addedToExistingPackage = true
+    //             break
+    //         }
+    //     }
+    //     if (!addedToExistingPackage) {
+    //         console.log(JSON.stringify(currentItem))
+    //     }
+    // }
 
     for (const packageItem of packages) {
         packageItem.courierPrice = courierCharge(packageItem.totalWeight)
