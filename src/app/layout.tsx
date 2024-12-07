b412e8469a19a4f4eb13b7320416e6247e94aeca
zulhijaya18 <32.zulhijaya@gmail.com>
2024-12-08 01:11:35 +0800

    fix initial page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,19 @@
 import type { Metadata } from "next"
 import "./globals.css"
-import { Home } from "../components/Home"
 
 export const metadata: Metadata = {
   title: "Online Store with Next.js",
   description: "for technical test at Code PRO",
 }
 
-export default function RootLayout() {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
-      <body>
-        <Home />
-      </body>
+      <body>{children}</body>
     </html>
   )
 }
